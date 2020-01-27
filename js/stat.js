e0de4c9753eb7f38f7a2d8dc02ea2d7566c11fe9
SldTir <54772323+SldTir@users.noreply.github.com>
2020-01-27 22:46:15 +0300

    Начал настраивать цикл for

--- js/stat.js
+++ js/stat.js
@@ -5,9 +5,13 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var GAP_TOP = 100;
+var GAP_RIGHT = 30;
 var FONT_GAP = 10;
 var TEXT_WIDTH = 50;
+var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
+var COLUMN_SPACE = 50;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -17,9 +21,9 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var toneGenerator = function () {
-  var a = Math.floor(Math.random() * 10);
-  var graphColor = 'hsla' + '(' + '246, ' + '199%, ' + '50%, ' + '0.' + a + ')';
-  return graphColor;
+  var a = Math.ceil(Math.random() * 10);
+    var graphColor = 'hsla' + '(' + '246, ' + '199%, ' + '50%, ' + '0.' + a + ')';
+    return graphColor;  
 };
 
 window.renderStatistics = function (ctx) {
@@ -31,15 +35,25 @@ window.renderStatistics = function (ctx) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  ctx.fillText('Вы', CLOUD_X + GAP * 3, 100 + BAR_HEIGHT + CLOUD_Y + FONT_GAP);
-  ctx.fillText('Иван', 180, 270);
-  ctx.fillText('Юлия', 230, 270);
+  var players = ['Вы', 'Иван', 'Юлия'];
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(CLOUD_X + GAP * 3, 100, 40, 150);
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP + BAR_HEIGHT + CLOUD_Y + FONT_GAP);
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)'; 
+    ctx.fillRect(CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP, BAR_WIDTH, BAR_HEIGHT);
+  }
+
+
+
+  ctx.fillText(players[i], CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP + BAR_HEIGHT + CLOUD_Y + FONT_GAP);
   ctx.fillStyle = toneGenerator();
-  ctx.fillRect(180, 100, 40, 150);
+  ctx.fillRect(CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP, BAR_WIDTH, BAR_HEIGHT);
+
+
+
+  ctx.fillText(players[i], CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP + BAR_HEIGHT + CLOUD_Y + FONT_GAP);
   ctx.fillStyle = toneGenerator();
-  ctx.fillRect(230, 100, 40, 150);
+  ctx.fillRect(CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP, BAR_WIDTH, BAR_HEIGHT);
 };
 
