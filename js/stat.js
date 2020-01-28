ed223d67744583d6e5d173d1f4d1d85e7aeb95a7
Sld Tir <pasha.tir1991@mail.ru>
2020-01-28 18:49:57 +0300

    Исправил замечания наставника 1

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,8 @@ var FONT_GAP = 10;
 var BAR_WIDTH = 40;
 var barHeight = 150;
 var COLUMN_SPACE = 50;
+var TEXT_COLOR = '#000';
+var FONT = '16px PT Mono';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -19,10 +21,18 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.strokeRect(x - 1, y - 1, CLOUD_WIDTH + 2, CLOUD_HEIGHT + 2);
 };
 
-var toneGenerator = function () {
-  var a = Math.ceil(Math.random() * 100);
-  var graphColor = 'hsl' + '(' + '246, ' + a + '%, ' + '50%' + ')';
+var saturationGenerator = function () {
+  var saturation = Math.ceil(Math.random() * 100);
+  var graphColor = 'hsl' + '(' + '246, ' + saturation + '%, ' + '50%' + ')';
   return graphColor;
+};
+
+var graphColorGenerator = function (ctx, names) {
+  if (names === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = saturationGenerator();
+  }
 };
 
 var getMaxElement = function (arr) {
@@ -37,26 +47,26 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var tableHeader = function (ctx, titleTextColor, titleFont) {
+  ctx.fillStyle = titleTextColor;
+  ctx.font = titleFont;
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, GAP * 4);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, GAP * 6);
+};
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  tableHeader(ctx, TEXT_COLOR, FONT);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = TEXT_COLOR;
     ctx.fillText(players[i], CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, GAP_TOP + barHeight + CLOUD_Y + FONT_GAP);
     ctx.fillText(Math.ceil(times[i]), CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, barHeight - (barHeight * times[i]) / maxTime + GAP_TOP - GAP);
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = toneGenerator();
-    }
+    graphColorGenerator(ctx, players[i]);
     ctx.fillRect(CLOUD_X + GAP_RIGHT + COLUMN_SPACE * i, barHeight - (barHeight * times[i]) / maxTime + GAP_TOP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
   }
 };
