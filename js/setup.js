6a6dfe52bffbead821dad9854d3f67e5680fff90
Sld Tir <pasha.tir1991@mail.ru>
2020-02-01 16:59:00 +0300

    Исправил замечания наставника

--- js/setup.js
+++ js/setup.js
@@ -2,9 +2,9 @@
 'use strict';
 var NUMBER_OF_WIZARDS = 4;
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктория', 'Юлия', 'Люпина', 'Вашингтон'];
-var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
@@ -14,26 +14,25 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var generatorArrayIndex = function (number) {
+var generatesAnIndexArray = function (number) {
   var randomArrayIndex = Math.floor(Math.random() * number);
   return randomArrayIndex;
 };
 
 var wizards = [];
-var createMage = function (numberMages) {
-  for (var i = 1; i <= numberMages; i++) {
+var createsMages = function (numberOfMages) {
+  for (var i = 1; i <= numberOfMages; i++) {
     var mage = {
-      name: WIZARD_NAMES[generatorArrayIndex(WIZARD_NAMES.length)],
-      surname: WIZARD_SURNAME[generatorArrayIndex(WIZARD_SURNAME.length)],
-      coatColor: COAT_COLOR[generatorArrayIndex(COAT_COLOR.length)],
-      eyesColor: EYES_COLOR[generatorArrayIndex(EYES_COLOR.length)]
+      name: WIZARD_NAMES[generatesAnIndexArray(WIZARD_NAMES.length)],
+      surname: WIZARD_SURNAMES[generatesAnIndexArray(WIZARD_SURNAMES.length)],
+      coatColor: COAT_COLORS[generatesAnIndexArray(COAT_COLORS.length)],
+      eyesColor: EYES_COLORS[generatesAnIndexArray(EYES_COLORS.length)]
     };
     wizards.push(mage);
   }
-  return wizards;
 };
 
-createMage(NUMBER_OF_WIZARDS);
+createsMages(NUMBER_OF_WIZARDS);
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
