0c23c5993622356680e432463720410e797d5781
Sld Tir <pasha.tir1991@mail.ru>
2020-03-01 21:42:54 +0300

    Сделал задание

---
+++ js/coloring-characters.js
@@ -0,0 +1,29 @@
+'use strict';
+(function () {
+  //Здесь я делаю рандомную расскраску персонажей
+
+  var setupWizard = document.querySelector('.setup-wizard');
+
+  var magesMantle = setupWizard.querySelector('.wizard-coat');
+
+  var eyeСolor = setupWizard.querySelector('.wizard-eyes');
+
+  var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
+
+  var inputSetupFireballWrap = setupFireballWrap.querySelector('input');
+
+  setupFireballWrap.addEventListener('click', function () {
+    var randomFireballColorValue = window.characteristicsMagician.COLOR_OF_FIREBALLS[window.randomNumber.generatesAnIndexArray(window.characteristicsMagician.COLOR_OF_FIREBALLS.length)];
+    setupFireballWrap.setAttribute('style', 'background-color: ' + randomFireballColorValue);
+    inputSetupFireballWrap.value = randomFireballColorValue;
+  });
+
+  magesMantle.addEventListener('click', function () {
+    magesMantle.style.fill = window.characteristicsMagician.COAT_COLORS[window.randomNumber.generatesAnIndexArray(window.characteristicsMagician.COAT_COLORS.length)];
+  });
+
+  eyeСolor.addEventListener('click', function () {
+    eyeСolor.style.fill = window.characteristicsMagician.EYES_COLORS[window.randomNumber.generatesAnIndexArray(window.characteristicsMagician.EYES_COLORS.length)];
+  });
+})();
+
---
+++ js/validation.js
@@ -0,0 +1,23 @@
+'use strict';
+(function () {
+  //Здесь я делаю валидацию
+  var setupUserName = document.querySelector('.setup-user-name');
+
+  setupUserName.addEventListener('invalid', function () {
+    if (setupUserName.validity.tooShort) {
+      setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (setupUserName.validity.tooLong) {
+      setupUserName.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (setupUserName.validity.valueMissing) {
+      setupUserName.setCustomValidity('Обязательное поле');
+    } else {
+      setupUserName.setCustomValidity('');
+    }
+  });
+
+  setupUserName.addEventListener('keydown', function (evt) {
+    if (evt.key === window.util.ESC_KEY) {
+      evt.stopPropagation();
+    }
+  });
+})();
